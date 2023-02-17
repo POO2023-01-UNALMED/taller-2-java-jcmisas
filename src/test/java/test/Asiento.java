159e0934912209b021cf6569981244921b71c554
jcmisas <jcmisas@hotmail.com>
2023-02-16 22:05:11 -0500

    desarollando las clases

--- src/test/java/test/Asiento.java
+++ src/test/java/test/Asiento.java
@@ -1,4 +1,28 @@
+package test.java.test;
+
 public class Asiento{
 
-    
+    String color;
+    int precio;
+    int registro;
+
+    void cambiarColor(String color){
+
+        if(color.equals("rojo")){
+            this.color=color;
+        }
+        else if(color.equals("verde")){
+            this.color=color;
+        }
+        else if(color.equals("amarillo")){
+            this.color=color;
+        }
+        else if(color.equals("negro")){
+            this.color=color;
+        }
+        else if(color.equals("blanco")){
+            this.color=color;
+        }
+        
+    }
 }
--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,4 +1,30 @@
+package test.java.test;
+
 public class Auto{
 
-    
+    String modelo;
+    int precio;
+    Asiento[] asientos;
+    String marca;
+    Motor motor;
+    int registro;
+    static int cantidadCreados;
+
+    int cantiadAsientos(){
+
+        return this.asientos.length;
+    }
+
+    String verificarIntegridad(){
+
+        if(this.registro==this.motor.registro==this.asientos.registro){
+
+            return "Auto original";
+        }
+        else{
+
+            return "Las piezas no son originales";
+        }
+    }
+
 }
